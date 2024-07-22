a9530dcf379ac5c5ea60b47d3c9cb779705ef120
GisailleLily <shellymi9@gmail.com>
2024-07-21 22:28:07 -0700

    Last Image Set

--- src/js/data/2018-02-22.js
+++ src/js/data/2018-02-22.js
@@ -44,16 +44,6 @@ dataSet[dataSetVersion].options = [
       { name: "Tsurugi", key: "tsu" },
     ]
   },
-  {
-    name: "Remove alternate forms",
-    key: "form",
-    tooltip: "Check this to include alternate form counterparts to characters."
-  },
-  {
-    name: "Remove different dimension duplicates",
-    key: "dim",
-    tooltip: "Check this to include alternate dimension counterparts to characters."
-  },
 ];
 
 dataSet[dataSetVersion].characterData = [
@@ -137,6 +127,216 @@ dataSet[dataSetVersion].characterData = [
   {
     name: "Doudanuki",
     img: "dou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Fudou",
+    img: "fudou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Fukushima",
+    img: "fuku_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Goke",
+    img: "goke_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Gokotai",
+    img: "gokotai_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Gotou",
+    img: "gotou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hacchou",
+    img: "hacc_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hachisuka",
+    img: "hach_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hakata",
+    img: "hakata_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hakusan",
+    img: "hakusan_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hanjin",
+    img: "hanjin_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Heishishourinken",
+    img: "heish_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hasebe",
+    img: "hesh_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Higekiri",
+    img: "hige_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Himetsuru",
+    img: "hime_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hirano",
+    img: "hirano_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hizamaru",
+    img: "hiza_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hizen",
+    img: "hizen_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Honebami",
+    img: "honebami_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Horikawa",
+    img: "hori_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hotarumaru",
+    img: "hotaru_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Hyuuga",
+    img: "hyuu_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Ichigo",
+    img: "ichigo_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Ichimonji",
+    img: "ichimonji_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Imanotsurugi",
+    img: "imano_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Houchou",
+    img: "hou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Inaba",
+    img: "inaba_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Ishida",
+    img: "ishida_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Ishikirimaru",
+    img: "ishiki_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Iwatooshi",
+    img: "iwatooshi_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kanesada",
+    img: "izumin_n.jpg",
     opts: {
 
     },
