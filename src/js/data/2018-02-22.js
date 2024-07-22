fbda13a58289b6dcf28e35a93cf3e2a0cd87ca4d
GisailleLily <shellymi9@gmail.com>
2024-07-21 23:03:04 -0700

    Missing Images

--- src/js/data/2018-02-22.js
+++ src/js/data/2018-02-22.js
@@ -341,4 +341,501 @@ dataSet[dataSetVersion].characterData = [
 
     },
   },
+  {
+    name: "Jikkyuu",
+    img: "jikk_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Jiroutachi",
+    img: "jirou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Jizou Yukihira",
+    img: "jizou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Juzumaru",
+    img: "juzu_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kanesada",
+    img: "izumin_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kasen",
+    img: "kasen_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kashagiri",
+    img: "kasha_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kashuu",
+    img: "kashii_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kenshin",
+    img: "kenshin_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kikkou",
+    img: "kikkou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kogarasumaru",
+    img: "koga_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kogitsunemaru",
+    img: "kogi_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kokindenjunotachi",
+    img: "kokin_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Koryuu",
+    img: "koryuu_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kotegiri Gou",
+    img: "kotegiri_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kousetsu",
+    img: "kou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kuwana Gou",
+    img: "kuwa_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Kyougoku Masamune",
+    img: "kyou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Maeda Toushirou",
+    img: "maeda_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Magoroku Kanemoto",
+    img: "mag_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Matsui Gou",
+    img: "mat_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Midare Toushirou",
+    img: "midare_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Mikazuki Munechika",
+    img: "mikazuki_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Minamoto Kiyomaro",
+    img: "mina_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Momoyoshi",
+    img: "mono_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Mouri",
+    img: "mouri_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Murakumo Gou",
+    img: "mura_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Mutsunokami",
+    img: "mutsu_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nagasone Kotetsu",
+    img: "naga_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nakigitsune",
+    img: "naki_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Namazuo Toushirou",
+    img: "nama_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nankaitarou Chouson",
+    img: "nank_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nansen Ichimonji",
+    img: "nan_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nenekirimaru",
+    img: "nene_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nikkari",
+    img: "nikk_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nikkou",
+    img: "nikkou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Ningen Mukotsu",
+    img: "ningen_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Nukemaru",
+    img: "nuke_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Onimaru Kunitsuna",
+    img: "oni_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Oochidori",
+    img: "oochi_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Oodenta Mitsuyo",
+    img: "ood_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Ookanehira",
+    img: "ookan_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Ookurikara",
+    img: "ooku_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Otegine",
+    img: "ote_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Samidare Gou",
+    img: "sami_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Sanchoumou",
+    img: "sanchou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Sasanuki",
+    img: "sasa_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Sayo Samonji",
+    img: "sayo_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Sengo Muramasa",
+    img: "sengo_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Shichiseiken",
+    img: "shic_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Shinano Toushirou",
+    img: "shin_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Shishiou",
+    img: "shis_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Shizukagata",
+    img: "shizu_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Shokudaikiri",
+    img: "shoku_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Sohayanotsurugi",
+    img: "soha_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Souza Samonji",
+    img: "souza_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Suishinshi Masahide",
+    img: "suish_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Taikei Naotane",
+    img: "taik_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Taikogane Sadamune",
+    img: "taiko_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Taikou Samonji",
+    img: "taikou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Taroutachi",
+    img: "tarou_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Tomita Gou",
+    img: "tom_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Tomoegata",
+    img: "tomo_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Tonbokiri",
+    img: "tonbo_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Tsurumaru",
+    img: "tsuru_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Uguisumaru",
+    img: "ugui_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Urashima Kotetsu",
+    img: "ura_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Yagen",
+    img: "yag_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Yamabushi Kunihiro",
+    img: "yama_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Yamanbagiri",
+    img: "yaman_n.jpg",
+    opts: {
+
+    },
+  },
+  {
+    name: "Yamatonokami",
+    img: "yamat_n.jpg",
+    opts: {
+
+    },
+  },
 ];
